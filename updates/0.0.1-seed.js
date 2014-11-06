319f7c14668e57d4440fedcdc2089ff9ac665358
Joss Mackison <joss.mackison@gmail.com>
2014-11-06 16:10:32 +1100

    More admins

--- updates/0.0.1-seed.js
+++ updates/0.0.1-seed.js
@@ -1,13 +1,13 @@
 exports.create = {
 	User: [
-		{ __ref: 'boris',   'name.first': 'Boris', 'name.last': 'Bozic',       email: 'boris@thinkmill.com.au', gender: 'male', state: 'active', password: 'pingpong champion' },
-		{ __ref: 'jed',     'name.first': 'Jed',   'name.last': 'Watson',      email: 'jed@thinkmill.com.au',   gender: 'male', state: 'active', password: 'pingpong champion' },
+		{ __ref: 'boris',   'name.first': 'Boris', 'name.last': 'Bozic',       email: 'boris@thinkmill.com.au', gender: 'male', state: 'active', password: 'pingpong champion', isAdmin: true },
+		{ __ref: 'jed',     'name.first': 'Jed',   'name.last': 'Watson',      email: 'jed@thinkmill.com.au',   gender: 'male', state: 'active', password: 'pingpong champion', isAdmin: true },
 		{ __ref: 'joss',    'name.first': 'Joss',  'name.last': 'Mackison',    email: 'joss@thinkmill.com.au',  gender: 'male', state: 'active', password: 'pingpong champion', isAdmin: true },
-		{ __ref: 'tom',     'name.first': 'Tom',   'name.last': 'Walker',      email: 'tom@thinkmill.com.au',   gender: 'male', state: 'active', password: 'pingpong champion' },
-		{ __ref: 'tuan',    'name.first': 'Tuan',  'name.last': 'Hoang',       email: 'tuan@thinkmill.com.au',  gender: 'male', state: 'active', password: 'pingpong champion' },
+		{ __ref: 'tom',     'name.first': 'Tom',   'name.last': 'Walker',      email: 'tom@thinkmill.com.au',   gender: 'male', state: 'active', password: 'pingpong champion', isAdmin: true },
+		{ __ref: 'tuan',    'name.first': 'Tuan',  'name.last': 'Hoang',       email: 'tuan@thinkmill.com.au',  gender: 'male', state: 'active', password: 'pingpong champion', isAdmin: true },
 		
 		{ __ref: 'joel',    'name.first': 'Joel',  'name.last': 'Agresta',     email: 'joel@disruptdevs.com',   gender: 'male', state: 'active', password: 'pingpong champion' },
-		{ __ref: 'john',    'name.first': 'John',  'name.last': 'Molomby',     email: 'john@disruptdevs.com',   gender: 'male', state: 'active', password: 'pingpong champion' },
+		{ __ref: 'john',    'name.first': 'John',  'name.last': 'Molomby',     email: 'john@disruptdevs.com',   gender: 'male', state: 'active', password: 'pingpong champion', isAdmin: true },
 		{ __ref: 'reuben',  'name.first': 'Reuben','name.last': 'Coppa',       email: 'reuben@disruptdevs.com', gender: 'male', state: 'active', password: 'pingpong champion' },
 		
 		{ __ref: 'david',   'name.first': 'David',   'name.last': 'Banham',    email: 'david@prismatik.com.au', gender: 'male', state: 'active', password: 'pingpong champion' },
